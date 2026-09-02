7f1dc65c53f79e2ab7b575cf889b3609d3eced8c
Erick Mora <m0r4.erick@gmail.com>
2026-09-01 21:32:44 -0600

    Instalar React Router y definir la ruta registro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Registro from "./paginas/Registro"
 
 function App() {
-  return <Registro />
+  return (
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route
+          path="/registro"
+          element={<Registro />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+  )
 }
 
 export default App
